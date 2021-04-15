25b8602b7f8fdbdff594956ce4a1576da38414e4
jempCode <joseeduar.martinezperez32@gmail.com>
2021-04-15 17:13:59 -0500

    feat: Creacion de Proyecto
    Se generan los archivos de rutas, corolador para el modelo de proyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,3 +1,74 @@
-exports.crearProyecto = async (res, res) => {
+const { validationResult } = require("express-validator");
+const Proyecto = require("../models/Proyecto");
+exports.crearProyecto = async (req, res) => {
 
+    // revisar si hay errores
+    const errores = validationResult(req);
+    if( !errores.isEmpty() ) {
+        return res.status(400).json({errores:errores.array()});
+    }
+
+    try {
+        // crear un nuevo proyecto
+        const proyecto = new Proyecto(req.body);
+        // Guardadndo el creador via JWT
+        proyecto. creador = req.usuario.id;
+
+        // guardando el proyecto
+        proyecto.save()
+        res.status(201).json(proyecto)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg: 'Hubo un error'})
+    }
+}
+
+// obtienes todos los proyectos del usuario actual
+exports.obtenerProyectos = async (req, res) => {
+    try {
+        const proyectos = await Proyecto.find({ creador: req.usuario.id }).sort({creado: -1});
+        res.status(200).json(proyectos);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({msg:'hubo un error'});
+    }
+}
+
+
+// actualiza un proyecto
+exports.actualizarProyecto = async (req,res)=> {
+    // revisar si hay errores
+    const errores = validationResult(req);
+    if( !errores.isEmpty() ) {
+        return res.status(400).json({errores:errores.array()});
+    }
+
+    // extraer la informacion del proyecto
+    const { nombre } = req.body;
+    const nuevoProyecto = {};
+
+    if(nombre) {
+        nuevoProyecto.nombre = nombre;
+    }
+
+    try {
+        
+        // revisar el id
+        let proyecto = await Proyecto.findById(req.params.id);
+        
+        // revisar si existe el prooyecto
+        if(!proyecto) {
+            return res.status(404).json({msg: 'Proyecto no encontrado'})   ;
+        }
+        // verificar el creador del proyecto
+        if(proyecto.creador.toString() !== req.usuario.id ) {
+            return res.status(401).json({mgs: 'No autorizado'});
+        }
+        // actualizar
+        // proyecto = await Proyecto.findByIdAndUpdate( id:{ req.params.id}, {$set: nuevoProyecto} )
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({msg:'Error en el servidor'});
+    }
 }
--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const proyectoController = require('../controllers/proyectoController');
-
+const auth = require('../middlewares/auth');
 const router = express.Router();
+const { check } = require('express-validator');
+
 
 //api/proyectos
-router.post('/', proyectoController.crearProyecto);
+router.post('/',
+            [
+                check('nombre','El nombre del proyecto es obligatorio').not().isEmpty()
+            ],
+            auth,
+            proyectoController.crearProyecto
+            );
+
+router.get('/',
+                auth,
+                proyectoController.obtenerProyectos
+            );
+
+router.put('/:id',
+            auth,
+            [
+                check('nombre','El nombre del proyecto es obligatorio').not().isEmpty()
+            ],            
+            proyectoController.actualizarProyecto
+        );
 
 module.exports = router;
